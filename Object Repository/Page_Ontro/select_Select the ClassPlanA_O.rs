340220c7b1636f1c209c063fa7ea6d07cd074ae7
venkat <venkat@192.168.0.6>
2018-06-11 17:55:37 +0530

    ccccc

---
+++ Object Repository/Page_Ontro/select_Select the ClassPlanA_O.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Select the ClassPlanA_O</name>
+   <tag></tag>
+   <elementGuidId>1579cdfd-639e-4d70-8eda-d5d9433c6310</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>custom-select form-control form-control</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Select the Class/PlanA_Onetime.C_Two MonthsD_3 MonthsZeeZoo_One YearMonthly CoachingjHvBafemale coachingbala21Footballcrashsummer_CloneGopi ClassYend Bad Classssmart classsnamrttravis sportsMOnthly _ w CLass date w 1mMonthlyMOnthly _ w CLass date TwiceAMonthMOnthly _ w CLass date w 1m.../MonthlyMonthlypMonthlyagfgshypb67b67b45Summer Cricketerstimeless plancrashsummerplane sports</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[1]/div[1]/div[@class=&quot;ContentPane d-flex&quot;]/div[@class=&quot;Content&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-12&quot;]/div[@class=&quot;d-flex align-items-center justify-content-between&quot;]/div[@class=&quot;d-flex flex-row justify-content-end&quot;]/span[@class=&quot;dropdown show&quot;]/div[@class=&quot;dropdown-menu dropdown-menu-right show&quot;]/div[@class=&quot;disabled dropdown-item&quot;]/select[@class=&quot;custom-select form-control form-control&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_Ontro/span_Enrollments.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Enrollments</name>
+   <tag></tag>
+   <elementGuidId>452324a4-f00d-49db-8a5c-c1c6843e8817</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sidebar-item-name</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Enrollments</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[1]/div[1]/div[@class=&quot;ContentPane d-flex&quot;]/div[@class=&quot;SideBar&quot;]/div[@class=&quot;d-flex flex-column&quot;]/a[@class=&quot;item&quot;]/span[@class=&quot;sidebar-item-name&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
